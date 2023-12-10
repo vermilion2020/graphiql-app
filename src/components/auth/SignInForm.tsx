baabc6d593ae32f663bce0b8bb003763624a212f
Mali-zi <mne310872@yandex.ru>
2023-12-10 17:23:29 +0300

    style: style forms

--- src/components/auth/SignInForm.tsx
+++ src/components/auth/SignInForm.tsx
@@ -1,14 +1,14 @@
 import React, { useState } from 'react';
 import { signInWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../../firebase';
-import { NavLink, useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 function SignInForm() {
   const navigate = useNavigate();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  const onLogin = (e: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     signInWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
@@ -26,43 +26,70 @@ function SignInForm() {
 
   return (
     <div className='col'>
-      <h2 className='text-primary'>Sign In Form</h2>
-      <form className='border-0 border-secondary rounded-3 shadow p-3' onSubmit={(e) => onLogin(e)}>
-        <div className='mb-3'>
-          <label htmlFor='email' className='form-label'>
-            Email address
-          </label>
-          <input
-            type='email'
-            id='email'
-            placeholder='Email address'
-            className='form-control'
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </div>
-        <div className='mb-3'>
-          <label htmlFor='password' className='form-label'>
-            Password
-          </label>
-          <input
-            type='password'
-            id='password'
-            placeholder='Password'
-            autoComplete='current-password'
-            className='form-control'
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
+      <div className='mx-auto w-96'>
+        <div className='flex min-h-full flex-col justify-center px-6 py-12 lg:px-8'>
+          <h2 className='mt-10 mb-4 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900'>
+            Sign in to your account
+          </h2>
+          <form
+            className='bg-white dark:bg-slate-900 rounded-lg p-6 border shadow-xl'
+            onSubmit={(e) => handleSubmit(e)}
+          >
+            <label htmlFor='email' className='block h-24 text-left'>
+              <span className='block text-sm font-medium text-gray-900'>Email address</span>
+              <input
+                type='email'
+                id='email'
+                placeholder='Email address'
+                className='mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+            focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
+            disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
+            invalid:border-pink-500 invalid:text-pink-600
+            focus:invalid:border-pink-500 focus:invalid:ring-pink-500'
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+            </label>
 
-        <div className='mb-3'>
-          <button type='submit' className='btn btn-primary'>
-            Sign In
-          </button>
+            <label htmlFor='password' className='block h-24 text-left'>
+              <div className='flex items-center justify-between'>
+                <label htmlFor='password' className='block text-sm font-medium text-gray-900'>
+                  Password
+                </label>
+                <div className='text-sm'>
+                  <Link to='#' className='font-semibold leading-6 ps-1'>
+                    Forgot password?
+                  </Link>
+                </div>
+              </div>
+              <input
+                type='password'
+                id='password'
+                placeholder='Password'
+                className='mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+            focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
+            disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
+            invalid:border-pink-500 invalid:text-pink-600
+            focus:invalid:border-pink-500 focus:invalid:ring-pink-500'
+                onChange={(e) => setPassword(e.target.value)}
+                required
+                autoComplete='current-password'
+              />
+            </label>
+
+            <button className='button' type='submit'>
+              Sign In
+            </button>
+          </form>
+          <p className='mt-3'>
+            No account yet?
+            <Link to='/sign-up' className='font-semibold leading-6 ps-1'>
+              Sign Up
+            </Link>
+          </p>
         </div>
-      </form>
-      <p className='mt-3'>
-        No account yet? <NavLink to='/sign-up'>Sign Up</NavLink>
-      </p>
+      </div>
     </div>
   );
 }
--- src/components/auth/SignUpForm.tsx
+++ src/components/auth/SignUpForm.tsx
@@ -29,58 +29,74 @@ function SignUpForm() {
   };
 
   return (
-    <div className="col">
-      <h2 className="text-primary">Sign Up Form</h2>
-      <form
-        className="border-0 border-secondary rounded-3 shadow p-3"
-        onSubmit={(e) => handleSubmit(e)}
-      >
-        <div className="mb-3">
-          <label htmlFor="email" className="form-label">
-            Email address
+    <div className='mx-auto w-96'>
+      <div className='flex min-h-full flex-col justify-center px-6 py-12 lg:px-8'>
+        <h2 className='mt-10 mb-4 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900'>
+          Create an account
+        </h2>
+        <form
+          className='bg-white dark:bg-slate-900 rounded-lg p-6 border shadow-xl'
+          onSubmit={(e) => handleSubmit(e)}
+        >
+          <label htmlFor='email' className='block h-24 text-left'>
+            <span className='block text-sm font-medium text-gray-900'>Email address</span>
+            <input
+              type='email'
+              id='email'
+              placeholder='Email address'
+              className='mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+            focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
+            disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
+            invalid:border-pink-500 invalid:text-pink-600
+            focus:invalid:border-pink-500 focus:invalid:ring-pink-500'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
           </label>
-          <input
-            type="email"
-            id="email"
-            placeholder="Email address"
-            className="form-control"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="password" className="form-label">
-            Create password
-          </label>
-          <input
-            type="password"
-            id="password"
-            placeholder="Password"
-            className="form-control"
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="confirmPassword" className="form-label">
-            Confirm password
-          </label>
-          <input
-            type="password"
-            id="confirmPassword"
-            placeholder="Confirm password"
-            className="form-control"
-          />
-        </div>
 
-        <button type="submit" className="btn btn-primary">
-          Sign Up
-        </button>
-      </form>
-      <p className="mt-3">
-        Already have an account? <NavLink to="/sign-in">Sign In</NavLink>
-      </p>
+          <label htmlFor='password' className='block h-24 text-left'>
+            <span className='block text-sm font-medium text-gray-900'>Create password</span>
+            <input
+              type='password'
+              id='password'
+              placeholder='Password'
+              className='mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+            focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
+            disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
+            invalid:border-pink-500 invalid:text-pink-600
+            focus:invalid:border-pink-500 focus:invalid:ring-pink-500'
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </label>
+
+          <label htmlFor='confirmPassword' className='block h-24 text-left'>
+            <span className='block text-sm font-medium text-gray-900'>Confirm password</span>
+            <input
+              type='password'
+              id='confirmPassword'
+              placeholder='Confirm password'
+              className='mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+            focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
+            disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
+            invalid:border-pink-500 invalid:text-pink-600
+            focus:invalid:border-pink-500 focus:invalid:ring-pink-500'
+              required
+            />
+          </label>
+
+          <button className='button' type='submit'>
+            Sign Up
+          </button>
+        </form>
+        <p className='mt-3'>
+          Already have an account?
+          <NavLink to='/sign-up' className='font-semibold leading-6 ps-1'>
+            Sign In
+          </NavLink>
+        </p>
+      </div>
     </div>
   );
 }
