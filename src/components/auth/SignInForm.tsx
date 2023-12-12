ee0ecd8402085623508d159571da5df08e412067
Mali-zi <mne310872@yandex.ru>
2023-12-12 11:15:11 +0300

    feat: add signIn and signUp translations

--- src/components/auth/SignInForm.tsx
+++ src/components/auth/SignInForm.tsx
@@ -4,7 +4,7 @@ import { auth } from '../../firebase';
 import { Link, useNavigate } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 import { useContext } from 'react';
-import { LocaleContext } from "../../context/LocaleContext";
+import { LocaleContext } from '../../context/LocaleContext';
 
 export interface ISignInForm {
   email: string;
@@ -33,9 +33,15 @@ function SignInForm() {
 
   const navigate = useNavigate();
   const [isDisabled, setIsDisabled] = useState(true);
-  const [session, setSession] = useState<ISession>({ status: 'no-authenticated', userId: null });
+  const [session, setSession] = useState<ISession>({
+    status: 'no-authenticated',
+    userId: null,
+  });
 
-  const onSubmit = ({ email, password }: ISignInForm, e?: React.BaseSyntheticEvent) => {
+  const onSubmit = (
+    { email, password }: ISignInForm,
+    e?: React.BaseSyntheticEvent
+  ) => {
     console.log('email', email);
 
     e?.preventDefault();
@@ -70,27 +76,29 @@ function SignInForm() {
   }, [formState]);
 
   return (
-    <div className='col'>
-      <div className='mx-auto w-96'>
-        <div className='flex min-h-full flex-col justify-center px-6 py-12 lg:px-8'>
-          <h2 className='mt-10 mb-4 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900'>
-            Sign in to your account
+    <div className="col">
+      <div className="mx-auto w-96">
+        <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+          <h2 className="mt-10 mb-4 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+            {texts.signIn.title}
           </h2>
           <form
-            className='bg-white dark:bg-slate-900 rounded-lg p-6 border shadow-xl'
+            className="bg-white dark:bg-slate-900 rounded-lg p-6 border shadow-xl"
             onSubmit={handleSubmit(onSubmit)}
           >
-            <label htmlFor='email' className='block h-24 text-left'>
-              <span className='block text-sm font-medium text-gray-900'>Email address</span>
+            <label htmlFor="email" className="block h-24 text-left">
+              <span className="block text-sm font-medium text-gray-900">
+                {texts.signIn.emailLabel}
+              </span>
               <input
-                type='email'
-                id='email'
-                placeholder='Email address'
-                className='mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+                type="email"
+                id="email"
+                placeholder={texts.signIn.emailPlaceholder}
+                className="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
             focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
             disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
             invalid:border-pink-500 invalid:text-pink-600
-            focus:invalid:border-pink-500 focus:invalid:ring-pink-500'
+            focus:invalid:border-pink-500 focus:invalid:ring-pink-500"
                 {...register('email', {
                   required: 'Email address is required',
                   pattern: {
@@ -99,54 +107,57 @@ function SignInForm() {
                   },
                 })}
               />
-              <div className='block text-xs font-medium text-red-600'>
+              <div className="block text-xs font-medium text-red-600">
                 {formState.errors.email && formState.errors.email.message}
               </div>
             </label>
 
-            <label htmlFor='password' className='block h-24 text-left'>
-              <div className='flex items-center justify-between'>
-                <label htmlFor='password' className='block text-sm font-medium text-gray-900'>
-                  Password
+            <label htmlFor="password" className="block h-24 text-left">
+              <div className="flex items-center justify-between">
+                <label
+                  htmlFor="password"
+                  className="block text-sm font-medium text-gray-900"
+                >
+                  {texts.signIn.passwordLabel}
                 </label>
-                <div className='text-sm'>
-                  <Link to='#' className='font-semibold leading-6 ps-1'>
+                <div className="text-sm">
+                  <Link to="#" className="font-semibold leading-6 ps-1">
                     Forgot password?
                   </Link>
                 </div>
               </div>
               <input
-                type='password'
-                id='password'
-                placeholder='Password'
-                className='mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+                type="password"
+                id="password"
+                placeholder={texts.signIn.passwordPlaceholder}
+                className="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
             focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
             disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
             invalid:border-pink-500 invalid:text-pink-600
-            focus:invalid:border-pink-500 focus:invalid:ring-pink-500'
-                autoComplete='current-password'
+            focus:invalid:border-pink-500 focus:invalid:ring-pink-500"
+                autoComplete="current-password"
                 {...register('password', { required: 'Password is required' })}
               />
-              <div className='block text-xs font-medium text-red-600'>
+              <div className="block text-xs font-medium text-red-600">
                 {formState.errors.password && formState.errors.password.message}
               </div>
             </label>
 
             <button
-              type='submit'
+              type="submit"
               disabled={isDisabled}
-              className='rounded-md bg-buttonBg-600 px-3 py-2 text-sm font-semibold 
+              className="rounded-md bg-buttonBg-600 px-3 py-2 text-sm font-semibold 
               text-white shadow-sm hover:bg-buttonBg-400 focus-visible:outline 
               focus-visible:outline-2 focus-visible:outline-offset-2 
-              focus-visible:outline-buttonBg-400 disabled:bg-disabledButton hover:bg-buttonBg-400'
+              focus-visible:outline-buttonBg-400 disabled:bg-disabledButton hover:bg-buttonBg-400"
             >
-              {texts.signIn.title}
+              {texts.signIn.btnText}
             </button>
           </form>
-          <p className='mt-3'>
-            No account yet?
-            <Link to='/sign-up' className='font-semibold leading-6 ps-1'>
-              Sign Up
+          <p className="mt-3">
+            {texts.signIn.question}
+            <Link to="/sign-up" className="font-semibold leading-6 ps-1">
+              {texts.signIn.linkText}
             </Link>
           </p>
         </div>
--- src/components/auth/SignUpForm.tsx
+++ src/components/auth/SignUpForm.tsx
@@ -6,7 +6,7 @@ import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { schema } from './Schema';
 import { useContext } from 'react';
-import { LocaleContext } from "../../context/LocaleContext";
+import { LocaleContext } from '../../context/LocaleContext';
 
 export interface IFormInput {
   email: string;
@@ -31,10 +31,13 @@ function SignUpForm() {
   const navigate = useNavigate();
   const [isDisabled, setIsDisabled] = useState(true);
 
-  const onSubmit = async (data: IFormInput, e?: React.BaseSyntheticEvent): Promise<void> => {
+  const onSubmit = async (
+    data: IFormInput,
+    e?: React.BaseSyntheticEvent
+  ): Promise<void> => {
     e?.preventDefault();
 
-    const {email, password} = data;
+    const { email, password } = data;
     await createUserWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
         // Signed in
@@ -49,7 +52,7 @@ function SignUpForm() {
         console.log(errorCode, errorMessage);
         // ..
       });
-      reset();
+    reset();
   };
 
   useEffect(() => {
@@ -61,104 +64,117 @@ function SignUpForm() {
   }, [formState]);
 
   return (
-    <div className='mx-auto w-96'>
-      <div className='flex min-h-full flex-col justify-center px-6 py-12 lg:px-8'>
-        <h2 className='mt-10 mb-4 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900'>
-          Create an account
+    <div className="mx-auto w-96">
+      <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+        <h2 className="mt-10 mb-4 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+          {texts.signUp.formTitle}
         </h2>
-        <form className='bg-white dark:bg-slate-900 rounded-lg p-6 border shadow-xl' onSubmit={handleSubmit(onSubmit)}>
-          <label htmlFor='email' className='block h-24 text-left'>
-            <span className='block text-sm font-medium text-gray-900'>Email address</span>
+        <form
+          className="bg-white dark:bg-slate-900 rounded-lg p-6 border shadow-xl"
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <label htmlFor="email" className="block h-24 text-left">
+            <span className="block text-sm font-medium text-gray-900">
+              {texts.signUp.emailLabel}
+            </span>
             <input
-              type='email'
-              id='email'
-              autoComplete='email'
-              placeholder='Email address'
-              className='mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+              type="email"
+              id="email"
+              autoComplete="email"
+              placeholder={texts.signUp.emailPlaceholder}
+              className="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
             focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
             disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
             invalid:border-pink-500 invalid:text-pink-600
-            focus:invalid:border-pink-500 focus:invalid:ring-pink-500'
+            focus:invalid:border-pink-500 focus:invalid:ring-pink-500"
               {...register('email')}
             />
-            <div className='block text-xs font-medium text-red-600'>
+            <div className="block text-xs font-medium text-red-600">
               {formState.errors.email && formState.errors.email.message}
             </div>
           </label>
 
-          <label htmlFor='password' className='block h-24 text-left'>
-            <span className='block text-sm font-medium text-gray-900'>Create password</span>
+          <label htmlFor="password" className="block h-24 text-left">
+            <span className="block text-sm font-medium text-gray-900">
+              {texts.signUp.passwordLabel}
+            </span>
             <input
-              type='password'
-              id='password'
-              autoComplete='new-password'
-              placeholder='Password'
-              className='mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+              type="password"
+              id="password"
+              autoComplete="new-password"
+              placeholder={texts.signUp.passwordPlaceholder}
+              className="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
             focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
             disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
             invalid:border-pink-500 invalid:text-pink-600
-            focus:invalid:border-pink-500 focus:invalid:ring-pink-500'
+            focus:invalid:border-pink-500 focus:invalid:ring-pink-500"
               {...register('password')}
             />
-            <div className='block text-xs font-medium text-red-600'>
+            <div className="block text-xs font-medium text-red-600">
               {formState.errors.password && formState.errors.password.message}
             </div>
           </label>
 
-          <label htmlFor='confirmPassword' className='block h-24 text-left'>
-            <span className='block text-sm font-medium text-gray-900'>Confirm password</span>
+          <label htmlFor="confirmPassword" className="block h-24 text-left">
+            <span className="block text-sm font-medium text-gray-900">
+              {texts.signUp.confirmPasswordLabel}
+            </span>
             <input
-              type='password'
-              id='confirmPassword'
-              placeholder='Confirm password'
-              className='mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+              type="password"
+              id="confirmPassword"
+              placeholder={texts.signUp.confirmPasswordPlaceholder}
+              className="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
             focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
             disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
             invalid:border-pink-500 invalid:text-pink-600
-            focus:invalid:border-pink-500 focus:invalid:ring-pink-500'
+            focus:invalid:border-pink-500 focus:invalid:ring-pink-500"
               {...register('confirmPassword')}
             />
-            <div className='block text-xs font-medium text-red-600'>
-              {formState.errors.confirmPassword && formState.errors.confirmPassword.message}
+            <div className="block text-xs font-medium text-red-600">
+              {formState.errors.confirmPassword &&
+                formState.errors.confirmPassword.message}
             </div>
           </label>
 
-          <div className='relative flex gap-x-3 h-16 text-left'>
-            <div className='flex h-6 items-center'>
+          <div className="relative flex gap-x-3 h-16 text-left">
+            <div className="flex h-6 items-center">
               <input
-                id='accept'
-                type='checkbox'
-                className='h-4 w-4 accent-buttonColor-400'
+                id="accept"
+                type="checkbox"
+                className="h-4 w-4 accent-buttonColor-400"
                 {...register('accept')}
               />
             </div>
-            <div className='text-sm leading-6'>
-              <label htmlFor='accept' className='font-medium text-sm text-gray-900'>
-                By signing up you agree to our Terms and conditions
+            <div className="text-sm leading-6">
+              <label
+                htmlFor="accept"
+                className="font-medium text-sm text-gray-900"
+              >
+                {texts.signUp.tacLabel}
               </label>
-              <div className='block text-xs font-medium text-red-600'>
+              <div className="block text-xs font-medium text-red-600">
                 {formState.errors.accept && formState.errors.accept.message}
               </div>
             </div>
           </div>
 
-          <div className='mt-4'>
+          <div className="mt-4">
             <button
-              type='submit'
+              type="submit"
               disabled={isDisabled}
-              className='rounded-md bg-buttonBg-600 px-3 py-2 text-sm font-semibold 
+              className="rounded-md bg-buttonBg-600 px-3 py-2 text-sm font-semibold 
               text-white shadow-sm hover:bg-buttonBg-400 focus-visible:outline 
               focus-visible:outline-2 focus-visible:outline-offset-2 
-              focus-visible:outline-buttonBg-400 disabled:bg-disabledButton hover:bg-buttonBg-400'
+              focus-visible:outline-buttonBg-400 disabled:bg-disabledButton hover:bg-buttonBg-400"
             >
               {texts.signUp.title}
             </button>
           </div>
         </form>
-        <p className='mt-3'>
-          Already have an account?
-          <Link to='/sign-in' className='font-semibold leading-6 ps-1'>
-            Sign In
+        <p className="mt-3">
+          {texts.signUp.question}
+          <Link to="/sign-in" className="font-semibold leading-6 ps-1">
+            {texts.signUp.linkText}
           </Link>
         </p>
       </div>
