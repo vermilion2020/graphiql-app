c7aaa4278b475157c938673b40bed437748d7275
Militsa Tuseeva <tuseevamilitsa@gmail.com>
2023-12-16 23:19:28 +0100

    fix: response tab

--- src/components/main/Response.tsx
+++ src/components/main/Response.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useContext, useState } from 'react';
+import { useContext } from 'react';
 import { LocaleContext } from '../../context/LocaleContext';
 import { useAppSelector } from '../../redux';
 import CodeMirror from '@uiw/react-codemirror';
@@ -10,16 +10,16 @@ function Response() {
     (state) => state.requestState
   );
 
-  const [data, setData] = useState(response);
-
-  const onChange = useCallback((val: string) => {
-    setData(val);
-  }, []);
-
   return (
     <>
       <h2>{texts.main.response}</h2>
-      <CodeMirror value={data} height="640px" className="border-gray-700 border-solid border-2 text-left" extensions={[javascript({ jsx: true })]} onChange={onChange} />
+      <CodeMirror
+        value={response}
+        readOnly
+        height="640px"
+        className="border-gray-700 border-solid border-2 text-left"
+        extensions={[javascript({ jsx: true })]}
+      />
     </>
   );
 }
