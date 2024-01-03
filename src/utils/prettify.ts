69b3a2b5eb335a66b30d6ab81519d575aa1d3168
Militsa Tuseeva <tuseevamilitsa@gmail.com>
2024-01-03 23:06:11 +0100

    refactor: improve prettify

--- src/utils/prettify.ts
+++ src/utils/prettify.ts
@@ -1,5 +1,4 @@
 const WH_SPACE = '\t';
-const TO_REMOVE = ['\n', '\r', '\t'];
 
 const addSpaces = (cnt: number) => {
   let acc = '';
@@ -9,42 +8,36 @@ const addSpaces = (cnt: number) => {
   return acc;
 };
 
-function removeDoubleSpaces(text: string) {
-  if (text.indexOf('  ') !== -1) {
-    text = text.replace(/ {1,}/g, ' ');
-    return removeDoubleSpaces(text);
-  }
-  return text;
-}
-
 export const prettifyQuery = (query: string) => {
   try {
-    const clear = query
-      .split('')
-      .filter((s) => !TO_REMOVE.includes(s))
-      .join('');
+    const clear = query.replace(/[\t|\r|\n]/gi, ' ');
 
     let firstI = clear.trim().split('{');
     const prefix = firstI[0].length ? firstI[0] : '';
-
+    let maxIndent = '';
     firstI = firstI.slice(1);
-    firstI = firstI.map((s, i) => ` {\r${addSpaces(i + 1)}${s}`);
+    firstI = firstI.map((s, i) => {
+      maxIndent = addSpaces(i + 1);
+      return ` {\r${maxIndent}${s}`
+    });
     firstI[0] = `${prefix}${firstI[0]}`;
+
+    let parameters = firstI[firstI.length - 1];
+    parameters = parameters.replace(/\s{1,}/g, ' ').replace(/\s}/g, '}').trim();
+    parameters = parameters.replace(/\s{1}/g, `\r${maxIndent}`);
+    firstI[firstI.length - 1] = parameters;
 
     const secondI = firstI.join('').trim().split('}');
     firstI = secondI.map((s, i) =>
       i === 0 ? s : `\r${s}${addSpaces(secondI.length - i - 1)}}`
     );
 
-    let resultQuery = firstI
-      .join('')
-      .trim()
-      .split(':')
-      .join(': ')
-      .split('=')
-      .join(' = ');
+    let resultQuery = firstI.join('').trim();
+    
+    resultQuery = resultQuery.replace(/:/g, ': ');
+    resultQuery = resultQuery.replace(/=/g, ' = ');
 
-    resultQuery = removeDoubleSpaces(resultQuery);
+    resultQuery = resultQuery.replace(/ {1,}/g, ' ');
 
     return { status: 'ok', query: resultQuery };
   } catch {
