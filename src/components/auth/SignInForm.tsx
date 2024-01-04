716b4602bee3484044472639a91482f16173e885
Militsa Tuseeva <tuseevamilitsa@gmail.com>
2024-01-04 21:31:11 +0100

    fix: input width

--- src/components/auth/SignInForm.tsx
+++ src/components/auth/SignInForm.tsx
@@ -110,7 +110,7 @@ function SignInForm() {
               disabled={isLoading}
               placeholder={texts.signIn.emailPlaceholder}
               autoComplete="email"
-              className="mt-1 block w-full min-w-80 px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+              className="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
             focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
             disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
             invalid:border-pink-500 invalid:text-pink-600 
