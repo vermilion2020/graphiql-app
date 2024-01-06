d372866fe72486d1cb31224784c7864bf8d20214
Victor Saraev <vvsar@mail.ru>
2024-01-06 03:34:06 +0300

    feat: add tests for SignIn and SignUp

--- src/components/auth/SignInForm.test.tsx
+++ src/components/auth/SignInForm.test.tsx
@@ -1,22 +1,22 @@
 import { describe, it } from 'vitest';
 import { renderWithProviders } from '../../test-utils';
+import { screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import { store } from '../../redux';
 import { setSingIn } from '../../redux/features/appSlice';
 import { App } from '../../App';
 
-describe('Main page', async () => {
-  it('Welcome page is shown, when user is not signed in', async () => {
-    // Arrange
+describe('Sign in form', async () => {
+  it('Sign in form is rendered', async () => {
     store.dispatch(setSingIn(false));
     renderWithProviders(
-      <MemoryRouter initialEntries={['/main']}>
+      <MemoryRouter initialEntries={['/sign-in']}>
         <App />
       </MemoryRouter>,
       { store }
     );
 
-    // Expect
-    expect(location.pathname).toEqual('/');
+    expect(screen.getByText('Email address')).toBeInTheDocument();
+    expect(screen.getByText('Password')).toBeInTheDocument();
   });
 });
